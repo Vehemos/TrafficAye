438910d175ffaa7161e3fac50fbb91deffec5428
Sarthak Tripathi <sarthak.trip@gmail.com>
2018-11-25 07:17:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<script type='text/javascript' src='https://eu-west-1a.online.tableau.com/javascripts/api/viz_v1.js'></script><div class='tableauPlaceholder' style='width: 1920px; height: 849px;'><object class='tableauViz' width='1920' height='849' style='display:none;'><param name='host_url' value='https%3A%2F%2Feu-west-1a.online.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='&#47;t&#47;hackjunction' /><param name='name' value='7CityCompare&#47;Sheet1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='showAppBanner' value='false' /><param name='filter' value='iframeSizedToWindow=true' /></object></div>
 
 let sites = [
     { lat: 53.903192, lon: -1.629799, id: 1 },
@@ -68,4 +67,5 @@ function addMarkers(site) {
     
   
 }
+<script type='text/javascript' src='https://eu-west-1a.online.tableau.com/javascripts/api/viz_v1.js'></script><div class='tableauPlaceholder' style='width: 1920px; height: 849px;'><object class='tableauViz' width='1920' height='849' style='display:none;'><param name='host_url' value='https%3A%2F%2Feu-west-1a.online.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='&#47;t&#47;hackjunction' /><param name='name' value='7CityCompare&#47;Sheet1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='showAppBanner' value='false' /><param name='filter' value='iframeSizedToWindow=true' /></object></div>
 
