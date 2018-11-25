b3108f281c5f7e068e001c6e4c482911c326a6dc
Sarthak Tripathi <sarthak.trip@gmail.com>
2018-11-25 06:31:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 
 let sites = [
     { lat: 53.903192, lon: -1.629799, id: 1 },
-    { lat: 53.799186, lon: -1.534937, id: 4 },
+    { lat: 53.818928, lon: -1.603168, id: 2 },
+    { lat: 53.798325, lon: -1.525394, id: 3 },
+    { lat: 53.799408, lon: -1.535259, id: 4 },
     { lat: 53.796259, lon: -1.547696, id: 5 },
+    { lat: 53.796012, lon: -1.540937, id: 6 },
     { lat: 53.782096, lon: -1.559061, id: 7 },
 ]
 
@@ -12,9 +15,6 @@ currentSite = 7;
 let currentField = 'Co2'
 
 function loadData(siteId) {
-    function load() {
-        initChart(site_data[siteId], currentField);
-    }
     if (site_data[siteId]) {
         load();
         return;
@@ -47,58 +47,5 @@ function addMarkers(site) {
   win.focus();        })
         marker.addTo(mymap);
     })
-
-    // mymap.panTo([points[0][0], points[0][1]]);
 }
 
-let chart = null;
-
-function initChart(data, field) {
-    let points = data.map(p => p[field])
-    let counts = Object.entries(_.countBy(points))
-    if (field !== 'Co2') {
-        counts = counts.sort(([ka, va], [kb, vb]) => {
-            return ka.localeCompare(kb);
-        })
-    }
-    let labels = counts.map(([k, v], i) => {
-        return k;
-    });
-    let values = counts.map(([k, v], i) => {
-        return v;
-    });
-    let ctx = document.getElementById("myChart");
-    if (chart != null) {
-        chart.destroy();
-    }
-    chart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: labels,
-            datasets: [{
-                data: values
-            }]
-        },
-        options: {
-            maintainAspectRatio: false,
-            legend: {
-                display: false
-            },
-            scaleShowValues: true,
-            scales: {
-                xAxes: [{
-                    ticks: {
-                        autoSkip: false
-                    }
-                }]
-            }
-        }
-    })
-}
-
-let dropdown = document.getElementById("dropdown");
-
-dropdown.addEventListener('change', e => {
-    currentField = e.target.value;
-    initChart(site_data[currentSite], currentField);
-})
